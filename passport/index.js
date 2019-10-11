12eac1f06edeb44e914c89d479c4c4f26a2e4095
choiheesu <dqdq4197@naver,com>
2019-10-11 17:45:59 +0900

    sns_updates

--- passport/index.js
+++ passport/index.js
@@ -6,9 +6,8 @@ module.exports = (passport) => {
   passport.serializeUser((user, done) => {
     done(null, user.id);
   });
-
   passport.deserializeUser((id, done) => {
-    User.find({ where: { id } })
+    User.findOne({ where: { id } })
       .then(user => done(null, user))
       .catch(err => done(err));
   });
