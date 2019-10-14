667a4c517dc1e3ef7b5b83c36875dc7f5a3dc901
choiheesu <dqdq4197@naver,com>
2019-10-14 19:50:19 +0900

    update

--- models/index.js
+++ models/index.js
@@ -2,11 +2,10 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const db = {};
-//
+
 const sequelize = new Sequelize(
   config.database, config.username, config.password, config,
 );
-//
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 db.User = require('./user')(sequelize, Sequelize);
