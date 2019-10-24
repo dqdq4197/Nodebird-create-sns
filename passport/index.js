2bb327b3826a053090242121c9ff02c095cbbb65
choiheesu <dqdq4197@naver.com>
2019-10-25 01:13:45 +0900

    init

--- passport/index.js
+++ passport/index.js
@@ -1,7 +1,7 @@
 const local = require('./localStrategy');
 const kakao = require('./kakaoStrategy');
 const { User } = require('../models');
-
+//
 module.exports = (passport) => {
   passport.serializeUser((user, done) => {
     done(null, user.id);
